9862c3a713ccf02badae901d2836ce0f654ef017
Anuj mishra <captainanuj2004@gmail.com>
2026-03-28 05:51:10 +0530

    added gitignore

--- obj/Debug/net10.0/TaskHub.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa0a870abbf616190e2cf3175a3bee7bf0042220")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
